edb07738cc0d6668f66657c98373bfbc995ff598
larry <xspanni@gmail.com>
2022-01-12 14:19:23 +0800

    处理命令行参数

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <boost/program_options.hpp>
 #include "process.h"
 #include "logger/logger.h"
+
+namespace program_options = boost::program_options;
 
 int main(int argc, char *argv[]) {
   std::cerr << "Hello Client" << std::endl;
@@ -8,6 +11,23 @@ int main(int argc, char *argv[]) {
     std::cerr << "参数数量有误" << std::endl;
     return 1;
   }
+
+  //声明需要的选项
+  program_options::options_description desc("Allowed options");
+  desc.add_options()
+      ("help,h", "显示帮助信息")
+      ("verbose,v", "显示明细")
+      ("person,p", program_options::value<std::string>()->default_value("world"), "who");
+
+  program_options::variables_map vm;
+  store(parse_command_line(argc, argv, desc), vm);
+  notify(vm);
+
+  if (vm.count("help")) {
+    std::cout << desc;
+    return 0;
+  }
+  std::cout << "Hello " << vm["person"].as<std::string>() << std::endl;
 
   logger::log_trace("hello");
   logger::log_debug("hello");
